e89d1d54fd73f7d65e61324a077f83be8664c17e
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-03-02 15:17:04 +0000

    Automatically backed up by Learn

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -70,7 +70,7 @@ describe "updating data in a hash" do
 
     it "if key is present, returns a hash where the key's value is incremented by 1" do
       expect(update_counting_hash({count: 5},:count)).to eq({count: 6})
-      expect(update_counting_hash({'number' => 1, 'amount' => 50},'number')).to eq({'number' => 2, 'amount' => 51})
+      expect(update_counting_hash({'number' => 1, 'amount' => 50},'number')).to eq({'number' => 2, 'amount' => 50})
     end
   end
 
